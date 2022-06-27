67f20ba8b323ceb29208a99ccef97068da54fdf6
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2022-06-27 12:51:51 +0300

    getir herşeyi

--- src/routes/moneycase.routes.js
+++ src/routes/moneycase.routes.js
@@ -3,19 +3,21 @@ const router = express.Router()
 const moneyCaseController = require('../controllers/moneycase.controller')
 const {
   verifyToken,
+  isAdmin,
   isCashierOrAdmin,
+  isMemberOrCashierOrAdmin
 } = require('../middlewares/auth')
 const {
   cacheAllHistories,
+  cacheMyHistories
 } = require('../middlewares/redis')
 const {
   checkHistory,
+  checkSendInvoiceToEmail
 } = require('../middlewares/formErrorHandling')
 router
-profile
-  .get('/', verifyToken, isCashierOrAdmin, cacheAllHistories, moneyCaseController.getAllMoneyCase)
-  .get('/casetatus', verifyToken, isCashierOrAdmin, cacheAllHistories, moneyCaseController.getCaseStatus)
-  .post('/', verifyToken, isCashierOrAdmin, checkHistory, moneyCaseController.insertMoneyCase)
-  .patch('/casetatus', verifyToken, isCashierOrAdmin, checkHistory, moneyCaseController.updateCaseStatus)
-
+.get('/', verifyToken, isCashierOrAdmin, cacheAllHistories, moneyCaseController.getAllMoneyCase)
+.get('/casetatus', verifyToken, isCashierOrAdmin, cacheAllHistories, moneyCaseController.getAllMoneyCase)
+.post('/', verifyToken, isCashierOrAdmin, checkHistory, moneyCaseController.insertMoneyCase)
+.patch('/casetatus', verifyToken, isCashierOrAdmin, checkHistory, moneyCaseController.updateCaseStatus)
 module.exports = router
