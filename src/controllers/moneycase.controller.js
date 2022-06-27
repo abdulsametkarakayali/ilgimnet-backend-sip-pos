ad313923942f9a4b56fe651fc9bb53cbecfd142d
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2022-06-27 12:46:08 +0300

    getir herşeyi

--- src/controllers/moneycase.controller.js
+++ src/controllers/moneycase.controller.js
@@ -17,7 +17,7 @@ const MoneyCase = {
     MoneyCaseModels.getCaseStatus()
       .then(response => {
         const resultMoneyCaseStatus = response
-        helpers.redisInstance().setex('getAllMoneyCases', 60 * 60 * 12, JSON.stringify(resultMoneyCaseStatus))
+        helpers.redisInstance().setex('getCaseStatus', 60 * 60 * 12, JSON.stringify(resultMoneyCaseStatus))
         helpers.response(res, resultMoneyCaseStatus, res.statusCode, helpers.status.found, null)
       }).catch(err => {
         helpers.response(res, [], err.statusCode, null, null, err)
