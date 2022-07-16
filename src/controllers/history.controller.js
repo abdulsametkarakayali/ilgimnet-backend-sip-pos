3cdf5ac3b34e4a1835714ac835ece84ad51e2e6b
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2022-07-16 18:10:51 +0300

    getir herşeyi

--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -7,7 +7,7 @@ const history = {
     historyModels.getAllHistory(order)
       .then(response => {
         const resultHistory = response
-        helpers.redisInstance().setex('getAllHistories', 1 * 1 * 1, JSON.stringify(resultHistory))
+        helpers.redisInstance().setex('getAllHistories', 60 * 60 * 12, JSON.stringify(resultHistory))
         helpers.response(res, resultHistory, res.statusCode, helpers.status.found, null)
       }).catch(err => {
         helpers.response(res, [], err.statusCode, null, null, err)
