29f9c57628ebef91c876530700e966a3038d0f87
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2022-07-26 21:37:28 +0300

    getir herşeyi

--- src/models/history.model.js
+++ src/models/history.model.js
@@ -8,7 +8,7 @@ const history = {
     console.log("işlem başlatıldı sorgu")
     var d = new Date() // Today!
     console.log(day+1, "tarih testi")
-    return queryHelper('select * , sum(quantity) as salestotal  from  orderdetails o INNER JOIN histories h on o.orderDetailID  = h.id where  ( h.historyDate  BETWEEN '+ d.setDate(d.getDate() - 1) - 1+' AND '+ d+')' )
+    return queryHelper('select * , sum(quantity) as salestotal  from  orderdetails o INNER JOIN histories h on o.orderDetailID  = h.id where  ( h.historyDate  BETWEEN '+ d.setDate(d.getDate() - 1)+' AND '+ d+')' )
   },
   getMyHistory: (order, id) => {
     return queryHelper(`SELECT histories.*, users.name as cashier FROM histories JOIN users WHERE histories.idUser = users.id AND isMember = ${id} ORDER BY id ${!order ? 'desc' : order}`)
