38c9e7688bce8a6dac800eb0c15c02ba30c11021
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2022-08-14 16:06:13 +0300

    getir herşeyi

--- src/models/moneycase.model.js
+++ src/models/moneycase.model.js
@@ -5,7 +5,7 @@ const MoneyCase = {
     return queryHelper('SELECT * FROM moneycase order by Id DESC')
   },
   getLastInsertId: () => {
-    return queryHelper(' SELECT LAST_INSERT_ID( ) from moneycase limit 1')
+    return queryHelper('SELECT TOP 1 ,id FROM moneycase order by Id desc;')
   },
   getCaseStatus: () => {
     return queryHelper('SELECT * FROM casestatus')
