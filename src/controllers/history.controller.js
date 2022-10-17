74429d0b946cb41655881ee1489863640be9a44b
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2022-10-17 13:05:51 +0300

    ‘msg’

--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -56,6 +56,7 @@ const history = {
     moneyCaseModels.getLastInsertId()
     .then(response => {
       shiftHistory = response[0].id
+      console.log(response[0].id)
   }).catch(err => {
     console.log(err)
     helpers.response(res, [], err.statusCode, null, null, err)
